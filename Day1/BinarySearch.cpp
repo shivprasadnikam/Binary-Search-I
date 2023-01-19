ffe7bf6366ca0dd3962570e6341f15687716d5bf
Shivprasad Nikam <116971586+shivprasadnikam@users.noreply.github.com>
2023-01-19 23:17:26 +0530

    Create BinarySearch.cpp

---
+++ Day1/BinarySearch.cpp
@@ -0,0 +1,29 @@
+
+                                                          // Binary Search  //
+                                                          
+                                                          
+ class Solution {
+public:
+    int search(vector<int>& nums, int target) {
+        int size=nums.size();
+        int start=0;
+        int end=size-1;
+        
+
+        while(start<=end)
+        {
+            int mid=(start+end)/2;
+             
+            if(target==nums[mid])
+            return mid;
+            else if(target<nums[mid])
+            end=mid-1;
+            else
+            start=mid+1;
+        }
+        
+    
+        return -1;
+    }
+    
+};
