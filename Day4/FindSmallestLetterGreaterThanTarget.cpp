834a3c47772b9c9e3754617b132a5ccd3c6df0a0
Shivprasad Nikam <116971586+shivprasadnikam@users.noreply.github.com>
2023-01-22 09:02:05 +0530

    Create FindSmallestLetterGreaterThanTarget.cpp

---
+++ Day4/FindSmallestLetterGreaterThanTarget.cpp
@@ -0,0 +1,16 @@
+
+
+                                                              // Find Smallest Letter Greater Than Target //
+
+
+class Solution {
+public:
+    char nextGreatestLetter(vector<char>& letters, char target) {
+        for(int i=0; i<letters.size(); i++)
+        {
+            if(int(target)<int(letters[i]))
+            return letters[i];
+        }
+        return letters[0];
+    }
+};
