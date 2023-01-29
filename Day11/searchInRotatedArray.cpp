571079cb74de33f3d6541ee0dd3f5132e0969088
Shivprasad Nikam <116971586+shivprasadnikam@users.noreply.github.com>
2023-01-29 17:05:21 +0530

    Create searchInRotatedArray.cpp

---
+++ Day11/searchInRotatedArray.cpp
@@ -0,0 +1,16 @@
+
+
+
+                                                        // Search In rotated Array  //
+
+class Solution {
+public:
+    int search(vector<int>& nums, int target) {
+       for(int i=0; i<nums.size(); i++)
+        {
+            if(nums[i]==target)
+                return i;
+        }
+       return -1;
+    }
+};
